1c1db347eb41baaf0367dd0cfbb3bf04d6007fae
cvalenciaamao <cvalenciaamao@gmail.com>
2021-11-15 10:06:12 -0500

    primera carga

---
+++ resources/views/formularioprincipal.blade.php
@@ -0,0 +1,5 @@
+@extends('page')
+@section('campo2')
+ESPACIO PARA TEXTO DEL CAMPO2
+    
+@endsection
---
+++ resources/views/page.blade.php
@@ -0,0 +1,6 @@
+<br> HOTEL SAN BLAS <br>
+<br> INICIO <br>
+@yield('campo1')
+<section>PAGINA PRINCIPAL</section>
+<br> PIE DE PAGINA <br>
+@yield('campo2')
